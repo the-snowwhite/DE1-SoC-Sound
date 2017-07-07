5f95751ab5c2968978d6a6fd75d9c8b850ec67ed
Michael Brown <producer@holotronic.dk>
2017-07-07 23:32:35 +0200

    Changed clk mux to fifo inputs, reduced i2s fifos to 1 pr playback / capture (44.1,48 khz stil works fine), and updated stp file
    Signed-off-by: Michael Brown <producer@holotronic.dk>

--- DE1_SOC_Linux_Audio/DE1_SOC_Linux_Audio.v
+++ DE1_SOC_Linux_Audio/DE1_SOC_Linux_Audio.v
@@ -329,72 +329,40 @@ assign GPIO_0[31]       = ~LCD_VSD;
 
 	);
 
-	wire i2s_playback_fifo_ack48;
-	wire i2s_data_out48;
-	i2s_shift_out i2s_shift_out48(
+	wire i2s_clk;
+	wire i2s_data_out;
+	assign i2s_clk = i2s_clkctrl_apb_0_conduit_clk_sel_48_44 ?
+		clock_bridge_44_out_clk_clk : clock_bridge_48_out_clk_clk;
+
+	i2s_shift_out i2s_shift_out(
 		.reset_n							(hps_0_h2f_reset_reset_n),
-		.clk								(clock_bridge_48_out_clk_clk),
+		.clk								(i2s_clk),
 
 		.fifo_right_data					(i2s_output_apb_0_playback_fifo_data[63:32]),
 		.fifo_left_data						(i2s_output_apb_0_playback_fifo_data[31:0]),
 		.fifo_ready							(~i2s_output_apb_0_playback_fifo_empty),
-		.fifo_ack							(i2s_playback_fifo_ack48),
+		.fifo_ack							(i2s_playback_fifo_ack),
 
 		.enable								(i2s_playback_enable),
 		.bclk								(i2s_clkctrl_apb_0_conduit_bclk),
 		.lrclk								(i2s_clkctrl_apb_0_conduit_playback_lrclk),
-		.data_out							(i2s_data_out48)
-	);
-	wire i2s_playback_fifo_ack44;
-	wire i2s_data_out44;
-	i2s_shift_out i2s_shift_out44(
-		.reset_n							(hps_0_h2f_reset_reset_n),
-		.clk								(clock_bridge_44_out_clk_clk),
-
-		.fifo_right_data					(i2s_output_apb_0_playback_fifo_data[63:32]),
-		.fifo_left_data						(i2s_output_apb_0_playback_fifo_data[31:0]),
-		.fifo_ready							(~i2s_output_apb_0_playback_fifo_empty),
-		.fifo_ack							(i2s_playback_fifo_ack44),
-
-		.enable								(i2s_playback_enable),
-		.bclk								(i2s_clkctrl_apb_0_conduit_bclk),
-		.lrclk								(i2s_clkctrl_apb_0_conduit_playback_lrclk),
-		.data_out							(i2s_data_out44)
+		.data_out							(i2s_data_out)
 	);
 
-	wire i2s_capture_fifo_write48;
-	wire i2s_data_in48;
-	wire [63:0] i2s_capture_fifo_data48;
-	i2s_shift_in i2s_shift_in48(
+	wire i2s_data_in;
+	i2s_shift_in i2s_shift_in(
 		.reset_n							(hps_0_h2f_reset_reset_n),
-		.clk								(clock_bridge_48_out_clk_clk),
+		.clk								(i2s_clk),
 
-		.fifo_right_data					(i2s_capture_fifo_data48[63:32]),
-		.fifo_left_data						(i2s_capture_fifo_data48[31:0]),
+		.fifo_right_data					(i2s_output_apb_0_capture_fifo_data[63:32]),
+		.fifo_left_data						(i2s_output_apb_0_capture_fifo_data[31:0]),
 		.fifo_ready							(~i2s_output_apb_0_capture_fifo_full),
-		.fifo_write							(i2s_capture_fifo_write48),
+		.fifo_write							(i2s_capture_fifo_write),
 
 		.enable								(i2s_capture_enable),
 		.bclk								(i2s_clkctrl_apb_0_conduit_bclk),
 		.lrclk								(i2s_clkctrl_apb_0_conduit_capture_lrclk),
-		.data_in							(i2s_data_in48)
-	);
-	wire i2s_capture_fifo_write44;
-	wire i2s_data_in44;
-	wire [63:0] i2s_capture_fifo_data44;
-	i2s_shift_in i2s_shift_in44(
-		.reset_n							(hps_0_h2f_reset_reset_n),
-		.clk								(clock_bridge_44_out_clk_clk),
-
-		.fifo_right_data					(i2s_capture_fifo_data44[63:32]),
-		.fifo_left_data						(i2s_capture_fifo_data44[31:0]),
-		.fifo_ready							(~i2s_output_apb_0_capture_fifo_full),
-		.fifo_write							(i2s_capture_fifo_write44),
-
-		.enable								(i2s_capture_enable),
-		.bclk								(i2s_clkctrl_apb_0_conduit_bclk),
-		.lrclk								(i2s_clkctrl_apb_0_conduit_capture_lrclk),
-		.data_in							(i2s_data_in44)
+		.data_in							(i2s_data_in)
 	);
 
 	// Combinatorics
@@ -402,10 +370,8 @@ assign GPIO_0[31]       = ~LCD_VSD;
 	assign i2s_playback_enable = i2s_output_apb_0_playback_dma_enable & ~i2s_output_apb_0_playback_fifo_empty;
 	assign i2s_capture_enable = i2s_output_apb_0_capture_dma_enable & ~i2s_output_apb_0_capture_fifo_full;
 
-	// Mux and sync fifo read ack
+	// Sync fifo read ack
 	reg [2:0] i2s_playback_fifo_ack_synchro;
-	assign i2s_playback_fifo_ack = i2s_clkctrl_apb_0_conduit_clk_sel_48_44 ?
-		i2s_playback_fifo_ack44 : i2s_playback_fifo_ack48;
 	always @(posedge clock_bridge_0_out_clk_clk or negedge hps_0_h2f_reset_reset_n)
 		if (~hps_0_h2f_reset_reset_n)
 			i2s_playback_fifo_ack_synchro <= 0;
@@ -414,10 +380,8 @@ assign GPIO_0[31]       = ~LCD_VSD;
 	assign i2s_output_apb_0_playback_fifo_read = i2s_playback_fifo_ack_synchro[2] & ~i2s_playback_fifo_ack_synchro[1];
 	assign i2s_output_apb_0_playback_fifo_clk = clock_bridge_0_out_clk_clk;
 
-	// Mux and sync fifo write
+	// Sync fifo write
 	reg [2:0] i2s_capture_fifo_write_synchro;
-	assign i2s_capture_fifo_write = i2s_clkctrl_apb_0_conduit_clk_sel_48_44 ?
-		i2s_capture_fifo_write44 : i2s_capture_fifo_write48;
 	always @(posedge clock_bridge_0_out_clk_clk or negedge hps_0_h2f_reset_reset_n)
 		if (~hps_0_h2f_reset_reset_n)
 			i2s_capture_fifo_write_synchro <= 0;
@@ -426,18 +390,12 @@ assign GPIO_0[31]       = ~LCD_VSD;
 	assign i2s_output_apb_0_capture_fifo_write = i2s_capture_fifo_write_synchro[2] & ~i2s_capture_fifo_write_synchro[1];
 	assign i2s_output_apb_0_capture_fifo_clk = clock_bridge_0_out_clk_clk;
 
-	// Mux capture data
-	assign i2s_output_apb_0_capture_fifo_data = i2s_clkctrl_apb_0_conduit_clk_sel_48_44 ?
-		i2s_capture_fifo_data48 : i2s_capture_fifo_data44;
-
-	// Mux out
-	assign AUD_DACDAT = i2s_clkctrl_apb_0_conduit_clk_sel_48_44 ? i2s_data_out44 : i2s_data_out48;
+	// Out
+	assign AUD_DACDAT = i2s_data_out;
 
 	// Audio input
-	assign i2s_data_in44 = AUD_ADCDAT;
-	assign i2s_data_in48 = AUD_ADCDAT;
-	//assign i2s_data_in44 = i2s_data_out44; // Loopback for testing
-	//assign i2s_data_in48 = i2s_data_out48; // Loopback for testing
+	assign i2s_data_in = AUD_ADCDAT;
+	//assign i2s_data_in = i2s_data_out; // Loopback for testing
 
 	// Audio clocks inouts
 	assign AUD_BCLK = i2s_clkctrl_apb_0_conduit_master_slave_mode ?
